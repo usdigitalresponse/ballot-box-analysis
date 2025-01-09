f110c8a9c1e296dac1e0c13f164fa4a111960cb3
Tyler Richardett <tyler.richardett@gmail.com>
2025-01-09 19:54:39 +0000

    Abstraction layer for adding ballot box locations to isochrone map

--- src/ballot_box_analysis/map.py
+++ src/ballot_box_analysis/map.py
@@ -88,7 +88,7 @@ class KeplerMapState(BaseModel):
 class KeplerVisibleLayerGroups(BaseModel):
     label: bool = True
     road: bool = True
-    border: bool = False
+    border: bool = True
     building: bool = True
     water: bool = True
     land: bool = True
@@ -138,10 +138,29 @@ class TravelTimeRadiusLayer(BaseModel):
     filters: list[MapFilter] | None = None
 
 
+class BallotBoxLayer(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    ballot_boxes: gpd.GeoDataFrame
+    tooltip_cols: list[str]
+    geojson_col: str = "geometry"
+    color: list[int, int, int] = [255, 255, 255]
+    is_visible: bool = True
+    vis_config: KeplerVisConfig = KeplerVisConfig(
+        radius=18,
+        opacity=0.8,
+        strokeOpacity=0.8,
+        thickness=0.8,
+        strokeColor=[0, 0, 0],
+        stroked=True,
+    )
+
+
 class KeplerMapLayerTitles(StrEnum):
     COUNTY_BOUNDARY = "County Boundary"
     VOTER_ADDRESS = "Voter Address"
     TRAVEL_TIME_RADIUS = "Travel Time Radius"
+    BALLOT_BOX = "Ballot Box"
 
 
 class InvalidCountyError(Exception):
@@ -233,11 +252,16 @@ class KeplerMap:
 
 class IsochroneMap(KeplerMap):
     def __init__(
-        self, county: str, voter_address_layer: VoterAddressLayer, travel_time_radius_layer: TravelTimeRadiusLayer
+        self,
+        county: str,
+        voter_address_layer: VoterAddressLayer,
+        travel_time_radius_layer: TravelTimeRadiusLayer,
+        ballot_box_layer: BallotBoxLayer,
     ) -> None:
         super().__init__(county)
         self._add_voter_address_layer(voter_address_layer)
         self._add_travel_time_radius_layer(travel_time_radius_layer)
+        self._add_ballot_box_layer(ballot_box_layer)
 
     def _add_voter_address_layer(self, voter_address_layer: VoterAddressLayer) -> None:
         self.config.visState.layers.insert(
@@ -260,7 +284,7 @@ class IsochroneMap(KeplerMap):
         ]
 
         self._update_map_config()
-        self.map.add_data(data=voter_address_layer.voter_addresses, name=KeplerMapLayerTitles.VOTER_ADDRESS)
+        self.map.add_data(data=voter_address_layer.voter_addresses.fillna(""), name=KeplerMapLayerTitles.VOTER_ADDRESS)
 
     def _add_travel_time_radius_layer(self, travel_time_radius_layer: TravelTimeRadiusLayer) -> None:
         self.config.visState.layers.insert(
@@ -295,5 +319,28 @@ class IsochroneMap(KeplerMap):
 
         self._update_map_config()
         self.map.add_data(
-            data=travel_time_radius_layer.ballot_box_isochrones, name=KeplerMapLayerTitles.TRAVEL_TIME_RADIUS
+            data=travel_time_radius_layer.ballot_box_isochrones.fillna(""), name=KeplerMapLayerTitles.TRAVEL_TIME_RADIUS
         )
+
+    def _add_ballot_box_layer(self, ballot_box_layer: BallotBoxLayer) -> None:
+        self.config.visState.layers.insert(
+            0,
+            KeplerLayer(
+                id=KeplerMapLayerTitles.BALLOT_BOX,
+                type="geojson",
+                config=KeplerLayerConfig(
+                    dataId=KeplerMapLayerTitles.BALLOT_BOX,
+                    label=KeplerMapLayerTitles.BALLOT_BOX,
+                    color=ballot_box_layer.color,
+                    columns=KeplerGeojsonColumns(),
+                    isVisible=ballot_box_layer.is_visible,
+                    visConfig=ballot_box_layer.vis_config,
+                ),
+            ),
+        )
+        self.config.visState.interactionConfig.tooltip.fieldsToShow[KeplerMapLayerTitles.BALLOT_BOX] = [
+            KeplerField(name=col) for col in ballot_box_layer.tooltip_cols
+        ]
+
+        self._update_map_config()
+        self.map.add_data(data=ballot_box_layer.ballot_boxes.fillna(""), name=KeplerMapLayerTitles.BALLOT_BOX)
